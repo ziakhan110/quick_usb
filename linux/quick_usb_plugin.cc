fc11e46e259a286a78a7cfd3e2b8a6d905a22580
ziakhan110 <zia.khan110@gmail.com>
2023-08-25 13:29:00 +0500

    first commit

---
+++ linux/quick_usb_plugin.cc
@@ -0,0 +1,4 @@
+#include "include/quick_usb/quick_usb_plugin.h"
+
+void quick_usb_plugin_register_with_registrar(FlPluginRegistrar* registrar) {
+}
---
+++ windows/quick_usb_plugin.cpp
@@ -0,0 +1,5 @@
+#include "include/quick_usb/quick_usb_plugin.h"
+
+void QuickUsbPluginRegisterWithRegistrar(
+    FlutterDesktopPluginRegistrarRef registrar) {
+}
